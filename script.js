cb3bb2656317b80f8b5dd32530c8df1334454461
Danilo <danilo.bakun@gmail.com>
2019-03-17 17:44:52 -0300

    habilidades melhoradas

--- script.js
+++
@@ -1,7 +0,0 @@
-function inicioMarginTop() {
-    document.getElementById("profileBackground").style.marginTop = document.getElementById('menu').clientHeight+"px";
-}
-
-function habilidadesMarginTop() {
-    document.getElementById("skills").style.marginTop = document.getElementById('menu').clientHeight+"px";
-}
